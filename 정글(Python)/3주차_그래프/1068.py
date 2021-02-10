c37588a30d677baefda251ec814fc72f8c68f6c4
tongky <chotjd329@hotmail.com>
2021-02-10 10:42:54 +0900

    solved

--- 정글(Python)/3주차_그래프/1068.py
+++ 정글(Python)/3주차_그래프/1068.py
@@ -2,11 +2,12 @@
 import sys
 
 N = int(sys.stdin.readline())
-A = list(sys.stdin.readline().split())
+A = list(map(int,sys.stdin.readline().split()))
 D = int(sys.stdin.readline())
 X = {}
 
 for i in range(N):
+    if i == D or A[i] == D: continue
     if A[i] not in X:
         X[A[i]] = [i]
         continue
@@ -18,14 +19,13 @@ ans = 0
 
 def count(a):
     global ans
-    if a == D: return
-    if str(a) not in X:
+    if a not in X:
         ans+=1
         return
-    for child in X[str(a)]:
+    for child in X[a]:
         count(child)
 
-if A[D] == 0:
+if -1 not in X:
     print(ans)
 else:
     count(-1)
