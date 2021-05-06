191009fa4ad8eb9512f3da6cbe1899ea02649293
chosh <chotjd329@hotmail.com>
2021-05-06 12:38:53 +0900

    solved

--- 대회,기출/카카오_2021_순위검색.py
+++ 대회,기출/카카오_2021_순위검색.py
@@ -1,48 +1,54 @@
-#순위검색
 def solution(info, query):
-    root = {}
     ans = []
+    A = ["cpp", "java", "python"]
+    B = ["backend", "frontend"]
+    C = ["junior", "senior"]
+    D = ["chicken", "pizza"]    
     
-    for Q in info:
-        Q = list(Q.split(' '))
-        Q = filter(lambda x : x != 'and',Q)
-        now = root
-        for index, cond in enumerate(Q):
-            if cond in now:
-                if index == 4:
-                    now[cond] +=1
-                now = now[cond]
-            else:
-                if index != 4:
-                    now[cond] = {}
-                    now = now[cond]
-                else:
-                    now[cond] = 1
-                
-    # print(root)
+    data = {}
     
-    for Q in query:
-        Q = list(Q.split(' '))
-        Q = filter(lambda x : x != 'and',Q)
+    for a in A:
+        for b in B:
+            for c in C:
+                for d in D:
+                    data[a+" "+b+" "+c+" "+d] = []
+    
+    for string in info:
+        temp = string.split()
+        data[" ".join(temp[0:4])].append(int(temp[4]))
+    
+    for d,k in data.items():
+        k.sort(reverse=True)
+    
+    # print(data)
+    
+    def check(i,A):
+        if i == "-":
+            return A
+        else:
+            return [i]
+        
+    def bisect(a,x):
+        cl = 0
+        cr = len(a)
+        while cl < cr:
+            mid = (cl+cr) //2
+            if a[mid] >= x: cl = mid + 1
+            else: cr = mid
+        return cl
+        
+    for string in query:
         cnt = 0
-        queue = []
-        queue.append(root)
-        for index, cond in enumerate(Q):
-            temp = []
-            
-            while(queue):
-                now = queue.pop()
-                if index == 4:
-                    for key,value in now.items():
-                        if int(key) >= int(cond):
-                            cnt += value
-                if cond == "-":
-                    for i in now:
-                        temp.append(now[i])
-                if cond in now:
-                    temp.append(now[cond])
-
-            queue = temp           
-              
+        temp = string.split(" ")
+        temp = list(filter(lambda x : x != "and",temp))
+    
+        for a in check(temp[0],A):
+            for b in check(temp[1],B):
+                for c in check(temp[2],C):
+                    for d in check(temp[3],D):
+                        if(data[a+" "+b+" "+c+" "+d]):
+                            index = bisect(data[a+" "+b+" "+c+" "+d],int(temp[4]))
+                            cnt += index
         ans.append(cnt)
-    return ans   
+                    
+    return ans
