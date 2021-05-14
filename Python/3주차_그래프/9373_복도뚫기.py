334c61854fae3910eca071ce626b9ccd036892a6
chosh <chotjd329@hotmail.com>
2021-05-14 23:40:13 +0900

    fixed

--- Python/3주차_그래프/9373_복도뚫기.py
+++ Python/3주차_그래프/9373_복도뚫기.py
@@ -1,63 +1,64 @@
 #복도뚫기
 import sys
+sys.setrecursionlimit(5000)
 
-def find(a):
-    if vis[a] == a:
+def find(a,vis):
+    if vis[a] < 0:
         return a
-    vis[a] = find(vis[a])
+    vis[a] = find(vis[a],vis)
     return vis[a]
 
-def union(a,b):
-    a = find(a)
-    b = find(b)
+def union(a,b,vis):
+    a = find(a,vis)
+    b = find(b,vis)
 
     if a == b:
         return False
     
-    vis[a] = b
+    if(vis[a]  < vis[b]):
+        vis[a] += vis[b];
+        vis[b] = a;
+    else:
+        vis[b] += vis[a];
+        vis[a] = b;
+
     return True
 
-def kruskal():
-    global ans
-    for d,a,b in B:
-        if union(a,b):
-            if find(M) == find(M+1) == find(a):
-                ans = min(d,ans)
-
-
-N = int(sys.stdin.readline())
-for _ in range(N):
+def solve():
     W = int(sys.stdin.readline())
     M = int(sys.stdin.readline())
     A = []
     B = []
     ans = W
-    vis = [i for i in range(M+2)]
+    vis = [-1 for i in range(M+2)]
 
     for i in range(M):
         A.append(list(map(int,sys.stdin.readline().split())) + [i])
 
     for i in range(M):
-        temp = A[i][0] - A[i][2]
-        if temp > 0:
-            B.append((temp,i,M))
-        else:
-            B.append((0,i,M))
+        dist = A[i][0] - A[i][2]
+        B.append((max(0,dist),i,M))
 
-        temp = W - (A[i][0] + A[i][2])
-        if temp > 0:  
-            B.append((temp,i,M+1))
-        else:
-            B.append((0,i,M+1))
+        dist = W - (A[i][0] + A[i][2])
+        B.append((max(0,dist),i,M+1))
 
-        for j in range(i+1,M):
-            temp = ((A[i][0]-A[j][0])**2 + (A[i][1]-A[j][1])**2)**0.5 - A[i][2] - A[j][2]
-            if temp > 0:
-                B.append((temp,i,j))
-            else:
-                B.append((0,i,j))
+        for j in range(i):
+            dist = ((A[i][0]-A[j][0])**2 + (A[i][1]-A[j][1])**2)**0.5 - A[i][2] - A[j][2]
+            B.append((max(0,dist),i,j))
 
     B.sort()
-    kruskal()
+    # print(B)
 
-    ans = print(round(ans/2,6))
+    for d,a,b in B:
+        if union(a,b,vis):
+            if find(M,vis) == find(M+1,vis):
+                ans = min(d,ans)
+                ans = print(round(ans/2,6))
+                break
+
+
+
+N = int(sys.stdin.readline())
+for _ in range(N):
+    solve()
+
