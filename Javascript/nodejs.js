96d28889ad3c5f63347e4fb7bc6f3f92bd3ee831
chosh <chotjd329@hotmail.com>
2022-01-12 22:09:16 +0900

    solved

---
+++ Javascript/nodejs.js
@@ -0,0 +1,7 @@
+// const line = require("fs").readFileSync("/dev/stdin", "utf8");
+const line = require("fs").readFileSync("./input.txt", "utf8");
+const input = line.trim().split("\n");
+
+const N = Number(input[0]);
+
+console.log(N);
